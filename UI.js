092bfc7b8f6a9cf506a07c056674dd971abfd8e8
SHuo Zhong <shuozhong258@gmail.com>
2024-10-17 21:35:55 +0800

    initial version of HK livability map

---
+++ UI.js
@@ -0,0 +1,32 @@
+const btnhide = document.getElementById('closeBtn');
+const nav = document.getElementById("nav");
+nav.style.display = 'block';
+
+btnhide.addEventListener('click', function () {
+    if (nav.style.display === 'none' || nav.style.display === '') {
+        // 如果导航栏已隐藏，重新显示并更新按钮文本
+        nav.style.display = 'block';
+        nav.style.opacity = 1; // 重置不透明度
+        btnhide.textContent = 'Hide'; // 更新按钮文本为“关闭”
+        btnhide.style.position = 'relative'; // 使按钮在导航栏内部
+    } else {
+        // 如果导航栏可见，则逐渐隐藏并更新按钮文本
+        var opacity = 1; // 初始不透明度
+        var fadeOutInterval = setInterval(function () {
+            if (opacity <= 0.05) {
+                clearInterval(fadeOutInterval);
+                nav.style.display = 'none'; // 隐藏元素
+                btnhide.className = "Hided"
+                btnhide.style.position = 'fixed'; // 移动按钮到右上角
+                btnhide.textContent = 'Display'; // 更新按钮文本为“显示”
+                btnhide.style.top = '20px'; // 设置按钮的顶部位置
+                btnhide.style.right = '20px'; // 设置按钮的右侧位置
+            } else {
+                opacity -= 0.1; // 每次减少不透明度
+                nav.style.opacity = opacity; // 更新元素不透明度
+            }
+        }, 10); // 每10毫秒执行一次
+
+
+    }
+});
---
+++ main.js
@@ -0,0 +1,18 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    projection: "EPSG:3857",
+    center: [12711171.903517054,2556018.552300771],
+    zoom: 11
+  })
+});
